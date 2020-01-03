8778d9ac35d7dd6dbc141a02a8a44ac6f542dd2c
ksn38 <40684564+ksn38@users.noreply.github.com>
2020-01-03 20:42:51 +0300

    Update vanga-1-trained.py

--- vanga-1-trained.py
+++ vanga-1-trained.py
@@ -18,59 +18,7 @@ class vanga:
         razmer_stupeni = 24
         k = 0.1
 
-        f = open('C:/MLProjects/venv7/vanga-1/btc/BTC-s2s.txt', 'w')
-
         a = pd.read_csv('C:/MLProjects/venv7/vanga-1/btc/BTC-USD.csv', sep=';', usecols=[1, 4])
-
-        list_chars = []
-
-        a['mid'] = (a['Open'] + a['Close']) / 2
-        var = ((a['Open'] - a['Close']) / a['mid'])
-        max_var = (max(var))*k
-        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                    'u', 'v', 'w', 'x', 'y', 'z']
-        char_indices = {i: c for i, c in enumerate(sorted(np.linspace(-max_var, max_var, len(alphabet))))}
-
-        def int_to_char(int_prev, int_next, int_mid):
-            sentence = []
-            var1 = (int_next - int_prev) / int_mid
-            for i in range(0, len(alphabet) - 1):
-                if char_indices[i] < var1 <= char_indices[i + 1]:
-                    sentence.append(alphabet[i])
-
-            sentence2 = (''.join(map(str, sentence)))
-            return sentence2
-
-        price_btc0 = []
-        price_btc1 = []
-
-        sentence3 = []
-        for stupenb in range(btc, razmer_stupeni + btc + 3):
-            chars = a.at[stupenb, 'Open']
-            chars2 = (a.at[stupenb, 'Close'])
-            chars3 = (a.at[stupenb, 'mid'])
-            price_btc0.append(chars3)
-            chars3_2 = (a.at[stupenb + razmer_stupeni - 1, 'mid'])
-            price_btc1.append(chars3_2)
-            sentence3.append(int_to_char(chars, chars2, chars3))
-        list_chars.append(''.join(map(str, sentence3)))
-
-        price_btc_to_itog = (';'.join(map(str, price_btc1)))
-
-        listik = []
-        for i in range(5, len(sentence3) + 1):
-            listik.append(''.join(map(str, sentence3[-i:])))
-
-        list_output = ['qwertyuiopasdfghjklzxcvbnm\t','qwertyuiopasdfghjklzxcvbnm\n']
-
-        for i in range(0, 18):
-            c = listik[i] + '\tqwertyuiopasdfghjklzxcvbnm\n'
-            list_output.append(c)
-
-        output_chars2 = (''.join(map(str, list_output)))
-
-        f.write(output_chars2)
-        f.close()
 
         batch_size = 64  # Batch size for training.
         epochs = 10  # Number of epochs to train for.
